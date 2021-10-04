9639fc63ba00bf157d1a7ed30d1b86815e968f16
roadisfirst <roadisfirst@gmail.com>
2021-10-04 20:18:24 +0300

    commit 1

--- client/src/app/features/steam/pages/profile/profile.component.ts
+++ client/src/app/features/steam/pages/profile/profile.component.ts
@@ -52,3 +52,5 @@ export class ProfileComponent implements OnInit {
   }
 
 }
+
+// changes enulation
--- client/src/app/features/steam/resolvers/user-profile.resolver.ts
+++ client/src/app/features/steam/resolvers/user-profile.resolver.ts
@@ -19,3 +19,5 @@ export class UserProfileResolver implements Resolve<User> {
   }
 
 }
+
+// changes enulation
